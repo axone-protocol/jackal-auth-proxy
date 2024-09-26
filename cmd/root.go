35126e434cda2006b63bc9c9c2f2bec7a14385c5
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-09-26 11:46:09 +0200

    feat: initiate cmd

---
+++ cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands.
+var rootCmd = &cobra.Command{
+	Use:   "jackal-auth-proxy",
+	Short: "Jackal connector to the OKP4 chain.",
+	Long:  "Jackal connector to the OKP4 chain.",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
